795d4cd167d7e033d227f7973dbec5fd2cd009e6
rohankaranje <149205870+rohankaranje@users.noreply.github.com>
2023-11-04 00:46:28 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,13 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/VJBots/Advance-File-Share-Bot /Advance-File-Share-Bot
+  git clone https://github.com/rohankaranje/Advance-File-Share-BotAnimeDekho /Advance-File-Share-BotAnimeDekho
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Advance-File-Share-Bot
+  git clone $UPSTREAM_REPO /Advance-File-Share-BotAnimeDekho
 fi
-cd /Advance-File-Share-Bot
+cd /Advance-File-Share-BotAnimeDekho
 pip3 install -U -r requirements.txt
 echo "Bot Started...."
 python3 bot.py
+
